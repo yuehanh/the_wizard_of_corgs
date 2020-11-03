320747fab8a499fc5742e600f167b9a67a647203
Yuehan <yuehan.huang@protonmail.com>
2020-11-03 00:55:14 -0800

    Set up main canvas

---
+++ src/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("canvas");
+  const bound = canvas.getBoundingClientRect();
+  canvas.width = bound.width;
+  canvas.height = bound.height;
+  const ctx = canvas.getContext("2d");
+});
