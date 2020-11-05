9bfc7f1e63d99ca9dfcd5c3b677f47ef647ebc11
Yuehan <yuehan.huang@protonmail.com>
2020-11-04 17:16:17 -0800

    Allow enemy class to draw health bar

--- src/enemies.js
+++ src/enemies.js
@@ -1,3 +1,6 @@
+import { HealthBar } from "./health_bar";
+import { Vector } from "./vector";
+
 export class Enemy {
   constructor() {
     this.health = ["hBar", "vBar", "/", "\\"];
@@ -5,7 +8,20 @@ export class Enemy {
     this.healthBar = document.createElement("healthbar");
     document.body.appendChild(this.body);
     this.body.appendChild(this.healthBar);
+    this.size = 20;
+    this.pos = new Vector(40, 40);
+    this.width = 20;
+    this.height = 40;
+    this.healthBar = new HealthBar(this);
     this.drawHealth();
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.pos.x, this.pos.y, this.width, this.height);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    this.healthBar.draw(ctx);
   }
   drawHealth() {
     this.healthBar.innerHTML = this.health.join(" ");
