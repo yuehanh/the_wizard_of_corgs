43ebcd88de91f13ad02ef54703f0b174b0239af8
Yuehan <yuehan.huang@protonmail.com>
2020-11-04 18:50:35 -0800

    Remove enemy when enemy healthbar run out

--- src/game.js
+++ src/game.js
@@ -1,18 +1,39 @@
+import { Command } from "./command";
 import { Enemy } from "./enemies";
 
 export class Game {
-  constructor(command) {
-    this.command;
+  constructor(canvas) {
     this.enemies = [];
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
   }
 
+  start() {
+    const command = new Command(canvas, this);
+    this.addEnemy();
+    this.draw();
+  }
   addEnemy() {
-    const enemy = new Enemy();
+    const enemy = new Enemy(this);
     this.enemies.push(enemy);
   }
-  receiveCommand(command) {
+  receiveCommand(direction) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     for (const enemy of this.enemies) {
-      enemy.update(command);
+      enemy.update(direction);
     }
+    this.draw();
+  }
+
+  draw() {
+    for (const enemy of this.enemies) {
+      enemy.draw(this.ctx);
+    }
+  }
+
+  remove(obj) {
+    debugger;
+    let idx = this.enemies.indexOf(obj);
+    this.enemies.splice(idx, 1);
   }
 }
