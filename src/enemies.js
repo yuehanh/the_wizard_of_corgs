89065b88a06d923657a167fe5d18a6b847af91a1
Yuehan <yuehan.huang@protonmail.com>
2020-11-05 13:52:00 -0800

    Add move function

--- src/enemies.js
+++ src/enemies.js
@@ -2,13 +2,13 @@ import { HealthBar } from "./health_bar";
 import { Vector } from "./vector";
 
 export class Enemy {
-  constructor(game) {
+  constructor(attr) {
     this.health = ["hBar", "vBar", "fSlash", "bSlash", "hBar"];
-    this.game = game;
-    this.size = 20;
-    this.pos = new Vector(40, 40);
-    this.width = 20;
-    this.height = 40;
+    this.game = attr.game;
+    this.pos = attr.pos;
+    this.vel = attr.velocity;
+    this.width = attr.width;
+    this.height = attr.height;
     this.healthBar = new HealthBar(this);
   }
 
@@ -24,6 +24,10 @@ export class Enemy {
     this.healthBar.draw(ctx);
   }
 
+  move() {
+    this.pos.add(this.vel);
+  }
+
   update(direction) {
     if (direction === this.health[0]) {
       this.health = this.health.slice(1);
@@ -35,5 +39,12 @@ export class Enemy {
 
   remove() {
     this.game.remove(this);
+  }
+
+  isCollidedWith(mainChar) {
+    this.pos.x < mainChar.pos.x + mainChar.width &&
+      this.pos.x + this.width > mainChar.pos.x &&
+      this.pos.y < mainChar.pos.y + mainChar.height &&
+      this.pos.y + this.height > mainChar.pos.y;
   }
 }
