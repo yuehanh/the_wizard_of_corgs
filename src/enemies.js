443bc7210bf461af234afe8a1ae5d10eb0a9fff7
Yuehan <yuehan.huang@protonmail.com>
2020-11-04 18:49:52 -0800

    Remove enemy when health bar run out

--- src/enemies.js
+++ src/enemies.js
@@ -2,18 +2,18 @@ import { HealthBar } from "./health_bar";
 import { Vector } from "./vector";
 
 export class Enemy {
-  constructor() {
-    this.health = ["hBar", "vBar", "/", "\\"];
+  constructor(game) {
+    this.health = ["hBar"];
     this.body = document.createElement("monster");
     this.healthBar = document.createElement("healthbar");
     document.body.appendChild(this.body);
+    this.game = game;
     this.body.appendChild(this.healthBar);
     this.size = 20;
     this.pos = new Vector(40, 40);
     this.width = 20;
     this.height = 40;
     this.healthBar = new HealthBar(this);
-    this.drawHealth();
   }
 
   draw(ctx) {
@@ -21,16 +21,25 @@ export class Enemy {
     ctx.rect(this.pos.x, this.pos.y, this.width, this.height);
     ctx.fillStyle = "red";
     ctx.fill();
-    this.healthBar.draw(ctx);
+    this.drawHealth(ctx);
   }
-  drawHealth() {
-    this.healthBar.innerHTML = this.health.join(" ");
+
+  drawHealth(ctx) {
+    this.healthBar.draw(ctx);
   }
 
   update(direction) {
+    debugger;
     if (direction === this.health[0]) {
       this.health = this.health.slice(1);
     }
-    this.drawHealth();
+    if (this.health.length === 0) {
+      this.remove();
+    }
+  }
+
+  remove() {
+    debugger;
+    this.game.remove(this);
   }
 }
