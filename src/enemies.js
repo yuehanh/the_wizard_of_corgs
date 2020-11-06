1cc0813957ac867f931a4d4c4ccbb23c07736c69
Yuehan <yuehan.huang@protonmail.com>
2020-11-06 14:07:40 -0800

    Randomize initial position of the enemy

--- src/enemies.js
+++ src/enemies.js
@@ -1,22 +1,45 @@
-import { HealthBar } from "./health_bar";
-import { Vector } from "./vector";
+import { HealthBar, HealthType } from "./health_bar";
+import { rndEntryPoint } from "./util";
 
 export class Enemy {
   constructor(attr) {
-    this.health = ["hBar", "vBar", "fSlash", "bSlash", "hBar"];
+    this.level = attr.level;
+    this.health = [];
     this.game = attr.game;
-    this.pos = attr.pos;
-    this.vel = attr.velocity;
     this.width = attr.width;
     this.height = attr.height;
     this.healthBar = new HealthBar(this);
+    this.targetPos = this.game.mainChar.pos;
+    this.image = attr.image;
+    this.status = true;
+    this.init();
+  }
+
+  init() {
+    let i = 0;
+    while (i < this.level) {
+      let rndIdx = Math.floor(Math.random() * HealthType.length);
+      this.health.push(HealthType[rndIdx]);
+      i++;
+    }
+    // this.pos = new Vector(40, 40);
+    this.pos = rndEntryPoint(this.game, this);
+    this.vel = this.targetPos.minus(this.pos);
+    this.vel.scale(1 / (this.vel.mag * 5));
   }
 
   draw(ctx) {
-    ctx.beginPath();
-    ctx.rect(this.pos.x, this.pos.y, this.width, this.height);
-    ctx.fillStyle = "red";
-    ctx.fill();
+    ctx.drawImage(
+      this.image,
+      0,
+      0,
+      48,
+      48,
+      this.pos.x,
+      this.pos.y,
+      this.width,
+      this.height
+    );
     this.drawHealth(ctx);
   }
 
@@ -25,7 +48,11 @@ export class Enemy {
   }
 
   move() {
-    this.pos.add(this.vel);
+    if (this.status) {
+      this.pos = this.pos.add(this.vel);
+    } else {
+      this.remove();
+    }
   }
 
   update(direction) {
@@ -33,7 +60,7 @@ export class Enemy {
       this.health = this.health.slice(1);
     }
     if (this.health.length === 0) {
-      this.remove();
+      this.status = false;
     }
   }
 
