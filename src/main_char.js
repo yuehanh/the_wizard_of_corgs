9ac45f63a872855c20f683e9b265e9c2ef82f56c
Yuehan <yuehan.huang@protonmail.com>
2020-11-06 17:22:13 -0800

    Add character response when hurt

--- src/main_char.js
+++ src/main_char.js
@@ -2,33 +2,41 @@ import { HealthBar } from "./health_bar";
 import { Vector } from "./vector";
 
 export class MainChar {
-  constructor(game) {
+  constructor(game, image) {
     this.OFFSET = 40;
     this.health = 5;
     this.game = game;
-    this.width = 40;
-    this.height = 40;
+    this.width = 60;
+    this.height = 60;
     this.pos = new Vector(
       game.width / 2 - this.width / 2,
       game.height - this.height - this.OFFSET
     );
+    this.image = image;
+    this.frameSpeed = 20;
   }
 
-  draw(ctx) {
-    ctx.beginPath();
-    ctx.rect(this.pos.x, this.pos.y, this.width, this.height);
-    ctx.fillStyle = "red";
-    ctx.fill();
+  draw(ctx, image, frame) {
+    ctx.drawImage(
+      image,
+      0 + 48 * (Math.floor(frame / this.frameSpeed) % 3),
+      336,
+      48,
+      48,
+      this.pos.x,
+      this.pos.y,
+      this.width,
+      this.height
+    );
     // this.drawHealth(ctx);
   }
 
-  // drawHealth(ctx) {
-  //   this.healthBar.draw(ctx);
-  // }
-
+  hurt() {
+    this.health -= 1;
+    this.frameSpeed = 2;
+    setTimeout(() => {
+      this.frameSpeed = 20;
+    }, 500);
+  }
   update() {}
-
-  // remove() {
-  //   this.game.remove(this);
-  // }
 }
