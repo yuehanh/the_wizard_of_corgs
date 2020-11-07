ad93dba90897e790cc67bf87b2941f6c3f1989db
Yuehan <yuehan.huang@protonmail.com>
2020-11-06 17:23:07 -0800

    Add image aggregate

---
+++ src/images.js
@@ -0,0 +1,11 @@
+export const background = new Image();
+background.src = "./image/background.png";
+
+export const ghostSprites = new Image();
+ghostSprites.src = "./image/ghosts.png";
+
+export const corgi = new Image();
+corgi.src = "./image/corgi.png";
+
+export const hearts = new Image();
+hearts.src = "./image/hearts.png";
