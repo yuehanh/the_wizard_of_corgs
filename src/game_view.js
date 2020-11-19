80f40749c4b0c41d345a258a38e5a885d1cc57a9
Yuehan <yuehan.huang@protonmail.com>
2020-11-18 16:32:28 -0800

    Add final score

--- src/game_view.js
+++ src/game_view.js
@@ -12,7 +12,8 @@ export class GameView {
     this.loadedImages = new Set();
     this.command = new Command(canvas, this.game);
     this.gameOverMenu = document.getElementById("game-over");
-    this.finalText = document.getElementById("final");
+    this.finalScore = document.getElementById("final-score");
+    this.score = document.getElementById("score");
     this.start();
   }
 
@@ -39,11 +40,11 @@ export class GameView {
     if (!this.game.gameOver && this.loaded()) {
       this.game.step(this.ctx, this.frameId);
     }
-
     this.frameId = requestAnimationFrame(this.animate);
+    this.score.innerText = this.game.score;
     if (this.game.gameOver) {
-      console.log("game Over");
       cancelAnimationFrame(this.frameId);
+      this.finalScore.innerText = this.game.score;
       this.gameOverMenu.classList.remove("hidden");
     }
   }
@@ -60,8 +61,8 @@ export class GameView {
     this.game = new Game(this.canvas);
     this.command.game = this.game;
     this.gameOverMenu.classList.add("hidden");
-    this.animate();
     this.game.addMainChar();
+    this.animate();
     this.startGame();
   }
 }
